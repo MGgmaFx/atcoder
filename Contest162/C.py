fe7ec29405152a884a416357341c8b8d2aaceff8
MGgmaFx <mggmafx@gmail.com>
2020-04-12 22:39:47 +0900

    Submission result AC

---
+++ Contest162/C.py
@@ -0,0 +1,10 @@
+N = int(input())
+S = input()
+ans = 0
+if N > 3:
+    for i in range(N - 2):
+        for j in range(i + 1, N - 1):
+            for k in range(j + 1, N):
+                if S[i] != S[j] and S[i] != S[k] and S[j] != S[k] and j - i != k - j:
+                    ans += 1
+print(ans)
